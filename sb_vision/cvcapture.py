a4bb0dfb8e7d31d827b3985fc5f5ece27fd9a411
Alistair Lynn <arplynn@gmail.com>
2017-07-27 16:42:14 +0100

    Docstrings for cvcapture

--- sb_vision/cvcapture.py
+++ sb_vision/cvcapture.py
@@ -1,10 +1,25 @@
+"""
+Low-level device capture utility.
+
+This builds upon some functionality sneaked in here from OpenCV.
+"""
+
 import threading
 
 from sb_vision.native import _cvcapture
 
 
 class CaptureDevice(object):
+    """A single device for capturing images."""
+
     def __init__(self, path=None):
+        """
+        General initialiser.
+
+        Where a device path is well-defined (for instance, v4l devices in
+        `/dev` on Linux) this can be passed in; otherwise a default device
+        is used).
+        """
         self.lock = threading.Lock()
         if path is not None:
             argument_c = _cvcapture.ffi.new(
@@ -18,6 +33,7 @@ class CaptureDevice(object):
             raise RuntimeError("Unable to open capture device")
 
     def capture(self, width, height):
+        """Capture a single image with the given width and height."""
         if self.instance is None:
             raise RuntimeError("capture device is closed")
 
@@ -39,12 +55,19 @@ class CaptureDevice(object):
         return bytes(_cvcapture.ffi.buffer(capture_buffer))
 
     def __enter__(self):
+        """Context manager protocol. Automatically closes on exit."""
         return self
 
     def __exit__(self, exc_value, exc_type, exc_traceback):
+        """Context manager protocol. Automatically closes on exit."""
         self.close()
 
     def close(self):
+        """
+        Close the device for further access.
+
+        This enables, for instance, other processes to use this device.
+        """
         if self.instance is not None:
             with self.lock:
                 _cvcapture.lib.cvclose(self.instance)
