a99bdd1cdc2aa9ca96b40df81140b2f0d074d3a3
Alistair Lynn <arplynn@gmail.com>
2017-07-27 16:22:24 +0100

    Add top level docstring

--- sb_vision/__init__.py
+++ sb_vision/__init__.py
@@ -1,3 +1,11 @@
+"""
+SourceBots vision system.
+
+Configure a source of images (a `Camera`, or a `FileCamera`), and
+process images into sets of `Token`s, representing the detection
+of AprilTags markers.
+"""
+
 from .vision import Vision
 from .camera import Camera, FileCamera
 from .tokens import Token
