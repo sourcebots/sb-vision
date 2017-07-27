9485d8320e3e8e8beb124d113dc6de7610737320
Alistair Lynn <arplynn@gmail.com>
2017-07-27 16:39:19 +0100

    Docstrings for `camera_base`

--- sb_vision/camera_base.py
+++ sb_vision/camera_base.py
@@ -8,15 +8,23 @@ import abc
 
 
 class CameraBase(metaclass=abc.ABCMeta):
+    """Base class for all cameras."""
+
     def __init__(self):
+        """Basic, general initialisation."""
         self.initialised = False
 
     def init(self):
-        """ Initialise the camera"""
+        """
+        Initialise the camera.
+
+        This is guaranteed to only be called once, and after it is called
+        the values for `focal_length` and `cam_image_size` must be set.
+        """
         self.initialised = True
 
     def get_image_size(self):
-        """ Get the size of images captured by the camera"""
+        """Get the size of images captured by the camera."""
         if not self.initialised:
             raise RuntimeError(
                 "Must Initialise camera before getting image size",
@@ -26,6 +34,8 @@ class CameraBase(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def capture_image(self):
         """
+        Capture a single image from this camera.
+
         :return: PIL Image captured
         """
         raise NotImplementedError()
