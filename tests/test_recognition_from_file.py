dde4d86010ffb2906ea3aa2a1cdb6a248848277a
Alistair Lynn <arplynn@gmail.com>
2017-07-27 16:11:39 +0100

    Parameterise this test

--- tests/test_recognition_from_file.py
+++ tests/test_recognition_from_file.py
@@ -1,12 +1,19 @@
+import pytest
+
 from pathlib import Path
 
 from sb_vision import Vision, FileCamera, Token
 
 TEST_DATA = Path(__file__).parent / 'test_data'
 
+TEST_IMAGES = [
+    ("Photo 1.jpg", [Token(id=9)]),
+]
 
-def test_recognises_basic_marker():
-    camera = FileCamera(TEST_DATA / 'Photo 1.jpg', 1.2)
+
+@pytest.mark.parametrize("photo, expected_tokens", TEST_IMAGES)
+def test_recognises_markers(photo, expected_tokens):
+    camera = FileCamera(TEST_DATA / photo, 1.2)
     vision = Vision(camera, 0.01)
     snaps = vision.snapshot()
-    assert snaps == [Token(id=9)]
+    assert snaps == expected_tokens
