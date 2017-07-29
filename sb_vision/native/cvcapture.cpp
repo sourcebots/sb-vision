a43aa2d8018ed423b19324206786e75088461da2
Alistair Lynn <arplynn@gmail.com>
2017-07-29 16:45:00 +0100

    Break out thresholding to a separate call

--- sb_vision/native/cvcapture.cpp
+++ sb_vision/native/cvcapture.cpp
@@ -8,6 +8,13 @@ extern "C" {
     void* cvopen(const char* path);
     void cvclose(void* context);
     int cvcapture(void* context, void* buffer, size_t width, size_t height);
+
+    void cvthreshold(
+        const void* src_buffer,
+        void* dst_buffer,
+        size_t width,
+        size_t height
+    );
 }
 
 #include "opencv2/opencv.hpp"
@@ -47,52 +54,84 @@ int cvcapture(void* context, void* buffer, size_t width, size_t height) {
         return 0;
     }
 
-    cv::Mat colour_image, greyscale_image, denoised_image, thresholded_image;
+    cv::Mat colour_image, greyscale_image;
 
     (*cap) >> colour_image;
     cv::cvtColor(colour_image, greyscale_image, cv::COLOR_BGR2GRAY);
-    cv::medianBlur(greyscale_image, denoised_image, 3);
-    if (!denoised_image.isContinuous()) {
+
+    if (!greyscale_image.isContinuous()) {
         return 0;
     }
     int died_horribly = 0;
-    if (denoised_image.size().width != width) {
+    if (greyscale_image.size().width != width) {
         fprintf(
             stderr,
             "Width mismatch: %d expected, %d actual\n",
             width,
-            denoised_image.size().width
+            greyscale_image.size().width
         );
         died_horribly = 1;
     }
-    if (denoised_image.size().height != height) {
+    if (greyscale_image.size().height != height) {
         fprintf(
             stderr,
             "Height mismatch: %d expected, %d actual\n",
             height,
-            denoised_image.size().height
+            greyscale_image.size().height
         );
         died_horribly = 1;
     }
     if (died_horribly) {
         return 0;
     }
+
+    memcpy(
+        buffer,
+        greyscale_image.ptr(),
+        width * height
+    );
+    return 1;
+}
+
+void cvthreshold(
+    const void* src_buffer,
+    void* dst_buffer,
+    size_t width,
+    size_t height
+) {
+    cv::Mat src(
+        static_cast<int>(width),
+        static_cast<int>(height),
+        CV_8UC1
+    );
+    cv::Mat denoised;
+    cv::Mat thresholded;
+
+    cv::medianBlur(src, denoised, 3);
+
+    memcpy(
+        src.ptr(),
+        src_buffer,
+        width * height
+    );
+
     size_t kernel_size = (width < height) ? width : height;
     kernel_size /= 2;
     kernel_size |= 1;
+
     cv::adaptiveThreshold(
-        denoised_image,
-        thresholded_image,
+        denoised,
+        thresholded,
         255.0,
         CV_ADAPTIVE_THRESH_MEAN_C,
         CV_THRESH_BINARY,
         kernel_size,
         0.0
     );
+
     memcpy(
-        buffer,
-        thresholded_image.ptr(),
+        dst_buffer,
+        thresholded.ptr(),
         width * height
     );
-    return 1;
 }
