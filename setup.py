6908b635c24f84546d081d7fdd23878d17fad378
Thomas Leese <thomas@leese.io>
2017-11-07 19:02:21 +0000

    Switch sb-vision version to 0.1.0
    It doesn't seem like it's at 1.0 yet.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sb-vision',
-    version='1.0',
+    version='0.1.0',
     description="Vision system for SourceBots",
     author="SourceBots",
     author_email='',
