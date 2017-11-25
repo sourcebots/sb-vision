009e9447fd512fddbdf58346f48c5eb9733b0e2d
Peter Law <PeterJCLaw@gmail.com>
2017-11-25 21:57:51 +0000

    Add capture script used today for testing cameras

--- sb_vision/camera.py
+++ sb_vision/camera.py
@@ -12,11 +12,13 @@ from sb_vision.cvcapture import CaptureDevice
 
 from .camera_base import CameraBase
 
+from typing import Tuple
+
 
 class Camera(CameraBase):
     """Actual camera, hooked up to a physical device."""
 
-    def __init__(self, device_id, proposed_image_size, distance_model):
+    def __init__(self, device_id: int, proposed_image_size: Tuple[int, int], distance_model: str):
         """Initialise camera with focal length and image size."""
         super().__init__()
         self.cam_image_size = proposed_image_size
---
+++ utils/capture_images.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import argparse
+import collections
+import re
+
+from PIL import Image
+
+from sb_vision.cvcapture import CaptureDevice
+
+
+class Resolution(collections.namedtuple('Resolution', ('width', 'height'))):
+    __slots__ = ()
+
+    @classmethod
+    def from_argument(cls, res):
+        try:
+            return cls.from_string(res)
+        except ValueError as e:
+            raise argparse.ArgumentTypeError(str(e))
+
+    @classmethod
+    def from_string(cls, res):
+        match = re.match(r'(?P<width>\d+)x(?P<height>\d+)', res)
+
+        if match is None:
+            raise ValueError(
+                "Invalid resolution specified. Must be in the form 'WIDTHxHEIGHT'"
+                " (got {})".format(repr(res)),
+            )
+
+        return cls(**{k: int(v) for k, v in match.groupdict().items()})
+
+    def __str__(self):
+        return '{0.width}x{0.height}'.format(self)
+
+
+def capture_image(device, image_size):
+    image_bytes = device.capture(*image_size)
+    return Image.frombytes('L', image_size, image_bytes)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('device_id', type=int)
+    parser.add_argument('image_template', type=str)
+    parser.add_argument(
+        '--resolution',
+        type=Resolution.from_argument,
+        default=Resolution(640, 480),
+        help="The resolution to capture from the device, default: %(default)s",
+    )
+    parser.add_argument(
+        '--num-images',
+        type=int,
+        default=2,
+        help="The number of images to capture, default: %(default)s",
+    )
+    return parser.parse_args()
+
+
+def main(args):
+    with CaptureDevice(args.device_id) as d:
+        for num in range(args.num_images):
+            print("Capturing image {}...".format(num), end='')
+            image = capture_image(d, args.resolution)
+
+            with open(args.image_template.format(num), mode='wb') as f:
+                image.save(f)
+
+            print("done")
+
+
+if __name__ == '__main__':
+    main(parse_args())
