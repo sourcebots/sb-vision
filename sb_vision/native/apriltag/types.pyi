40ae0b537b6c40d2400fa023fda0620079d5940f
Peter Law <PeterJCLaw@gmail.com>
2018-01-08 23:09:32 +0000

    Switch to class-based definitions of new types

--- sb_vision/native/apriltag/types.pyi
+++ sb_vision/native/apriltag/types.pyi
@@ -1,17 +1,14 @@
 from typing import List, NamedTuple
 
-MatrixData = NamedTuple('MatrixData', [
-    ('ncols', int),
-    ('nrows', int),
-    ('data', List[float]),
-])
+class MatrixData:
+    ncols: int
+    nrows: int
+    data: List[float]
 
 
-ApriltagDetection = NamedTuple('ApriltagDetection', [
-    ('id', int),
-    ('hamming', int),
-    ('goodness', float),
-    ('decision_margin', float),
-    ('H', MatrixData),
-    # Other fields omitted
-])
+class ApriltagDetection:
+    id: int
+    hamming: int
+    goodness: float
+    decision_margin: float
+    H: MatrixData
