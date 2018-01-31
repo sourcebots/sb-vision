ec1ac3b7c208ea3ca50351765d9e2d5212a0be0f
Peter Law <PeterJCLaw@gmail.com>
2018-01-31 18:48:03 +0000

    Sort imports

--- sb_vision/__init__.py
+++ sb_vision/__init__.py
@@ -6,9 +6,9 @@ process images into sets of `Token`s, representing the detection
 of AprilTags markers.
 """
 
-from .vision import Vision
 from .camera import Camera, FileCamera
 from .tokens import Token
+from .vision import Vision
 
 __all__ = [
     'Vision',
