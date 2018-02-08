1274f6a8be8210bcf1e5b1f2ae5db4e2a4a8cf96
Peter Law <PeterJCLaw@gmail.com>
2018-02-08 23:11:25 +0000

    Add some meaningful tests
    These demonstrate why it's safe to use a plain multiplicative
    conversion by using two images which are 1m and 2.5m away respectively,
    yet in the images it can plainly be seen that the makers occupy the
    same portion of the image.

--- tests/test_marker_sizes.py
+++ tests/test_marker_sizes.py
@@ -1,8 +1,20 @@
 """Tests for marker sizing."""
 
+from pathlib import Path
+from typing import Dict, Tuple
+from unittest import mock
+
 import numpy as np
 
-from sb_vision import Token
+from sb_vision import FileCamera, Token, Vision
+from sb_vision.camera_base import CameraBase
+
+TEST_DATA = Path(__file__).parent / 'test_data'
+
+# We're using TeckNet images with the c270 model, so these are expected to be a
+# bit off; note that they still preserve the ratio of 1:2.5 (0.7 : 1.75 then rounded)
+EXPECTED_LARGE_DISTANCE = 1.8
+EXPECTED_SMALL_DISTANCE = 0.7
 
 # Homography matrix of a real image.
 homography_from_camera = np.array([
@@ -44,3 +56,60 @@ def test_marker_size():
     assert round(big_x, 5) == round(small_x * ratio, 5), "Wrong x-coordinate"
     assert round(big_y, 5) == round(small_y * ratio, 5), "Wrong y-coordinate"
     assert round(big_z, 5) == round(small_z * ratio, 5), "Wrong z-coordinate"
+
+
+def assertMarkerDistance(
+    camera: CameraBase,
+    *,
+    marker_sizes: Dict[int, Tuple[float, float]],
+    expected_distance: float
+):
+    vision = Vision(camera)
+
+    with mock.patch('sb_vision.tokens.MARKER_SIZES', marker_sizes):
+        token, = vision.snapshot()
+
+    dist = token.spherical.dist
+    assert round(dist, 1) == expected_distance
+
+
+def test_unknown_marker_size():
+    # The c270 model is trained on 25cm markers; so it assume that all markers
+    # are that size unless told otherwise.
+    assertMarkerDistance(
+        FileCamera(TEST_DATA / 'tecknet-10cm-at-1m.jpg', distance_model='c270'),
+        marker_sizes={},
+        expected_distance=EXPECTED_LARGE_DISTANCE,
+    )
+
+
+def test_large_marker_large_size():
+    assertMarkerDistance(
+        FileCamera(TEST_DATA / 'tecknet-25cm-at-2.5m.jpg', distance_model='c270'),
+        marker_sizes={23: (0.25, 0.25)},
+        expected_distance=EXPECTED_LARGE_DISTANCE,
+    )
+
+
+def test_large_marker_small_size():
+    assertMarkerDistance(
+        FileCamera(TEST_DATA / 'tecknet-25cm-at-2.5m.jpg', distance_model='c270'),
+        marker_sizes={23: (0.1, 0.1)},
+        expected_distance=EXPECTED_SMALL_DISTANCE,
+    )
+
+
+def test_small_marker_large_size():
+    assertMarkerDistance(
+        FileCamera(TEST_DATA / 'tecknet-10cm-at-1m.jpg', distance_model='c270'),
+        marker_sizes={44: (0.25, 0.25)},
+        expected_distance=EXPECTED_LARGE_DISTANCE,
+    )
+
+
+def test_small_marker_small_size():
+    assertMarkerDistance(
+        FileCamera(TEST_DATA / 'tecknet-10cm-at-1m.jpg', distance_model='c270'),
+        marker_sizes={44: (0.1, 0.1)},
+        expected_distance=EXPECTED_SMALL_DISTANCE,
+    )
