b9a55a904d5376d5f5a225edffcb1a25a70bf861
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-02-18 21:05:02 +0000

    re-add LegacyPolar coordinate tests

--- tests/test_coordinates_from_file.py
+++ tests/test_coordinates_from_file.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import pytest
 from pytest import approx
 
-from sb_vision import Cartesian, FileCamera, Spherical, Vision
+from sb_vision import Cartesian, FileCamera, LegacyPolar, Spherical, Vision
 
 # Ensure all distance values are within this tolerance
 # (as a percentage of the total distance)
@@ -20,6 +20,11 @@ TEST_IMAGES = [
     (
         '1.0z-0.1x.jpg',
         Cartesian(x=-0.1, y=0, z=1),
+        LegacyPolar(
+            polar_x=1.6697,
+            polar_y=1.5740,
+            dist=(0.1**2 + 1**2) ** 0.5,  # or 1.004987562112089
+        ),
         Spherical(
             rot_x=0,
             rot_y=math.atan(-0.1),  # or -0.09966865249116204 radians
@@ -29,6 +34,11 @@ TEST_IMAGES = [
     (
         '3.0z1.0x.jpg',
         Cartesian(x=1, y=0, z=3),
+        LegacyPolar(
+            polar_x=1.1922,
+            polar_y=1.6032,
+            dist=(1**2 + 3**2) ** 0.5,  # or 3.1622776601683795
+        ),
         Spherical(
             rot_x=0,
             rot_y=math.atan(1 / 3),  # or 0.3217505543966422 radians
@@ -39,10 +49,10 @@ TEST_IMAGES = [
 
 
 @pytest.mark.parametrize(
-    "photo, expected_cartesian, expected_spherical",
+    "photo, expected_cartesian, expected_polar, expected_spherical",
     TEST_IMAGES,
 )
-def test_image_coordinates(photo, expected_cartesian, expected_spherical):
+def test_image_coordinates(photo, expected_cartesian, expected_polar, expected_spherical):
     """Make sure that this particular file gives these particular tokens."""
     camera = FileCamera(CALIBRATIONS / photo, camera_model='C016')
     vision = Vision(camera)
@@ -50,12 +60,17 @@ def test_image_coordinates(photo, expected_cartesian, expected_spherical):
 
     x, y, z = token.cartesian
     rot_x, rot_y, dist = token.spherical
+    polar_x, polar_y, polar_dist = token.legacy_polar
 
     tolerance = dist * DIST_PERCENT_TOLERANCE
 
     assert x == approx(expected_cartesian.x, abs=tolerance), "Wrong x-coordinate"
     assert y == approx(expected_cartesian.y, abs=tolerance), "Wrong y-coordinate"
     assert z == approx(expected_cartesian.z, abs=tolerance), "Wrong z-coordinate"
+
+    assert polar_x == approx(expected_polar.polar_x, abs=tolerance), "Wrong polar_x coordinate"
+    assert polar_y == approx(expected_polar.polar_y, abs=tolerance), "Wrong polar_y coordinate"
+    assert polar_dist == approx(expected_polar.dist, abs=tolerance), "Wrong polar_dist coordinate"
 
     assert rot_x == approx(expected_spherical.rot_x, abs=tolerance), "Wrong x-coordinate"
     assert rot_y == approx(expected_spherical.rot_y, abs=tolerance), "Wrong y-coordinate"
