Fix rotation of markers
Previously the rotation of markers was returned as 90 degrees
offset from their original angle. Change the expected shape
so that the expectation of markers is so that the first corner is
in the top left (as is returned in AprilTags)

sb_vision/find_3D_coords.py
@@ -108,10 +108,10 @@ def calculate_transforms(
 
     # create the rectangle representing the marker in 3D
     object_points = np.array([
+        [-width_from_centre, height_from_centre, 0],
         [width_from_centre, height_from_centre, 0],
         [width_from_centre, -height_from_centre, 0],
         [-width_from_centre, -height_from_centre, 0],
-        [-width_from_centre, height_from_centre, 0],
     ])
 
     return_value, orientation_vector, translation_vector = cv2.solvePnP(