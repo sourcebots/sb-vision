045c7d1141af3e4393b06e1a55e67bf376ba2ff0
Peter Law <PeterJCLaw@gmail.com>
2018-02-25 22:33:58 +0000

    Make this test actually useful

--- tests/test_load_calibrations.py
+++ tests/test_load_calibrations.py
@@ -29,4 +29,23 @@ def test_accepts_matching_resolution():
         'pathlib.Path.open',
         return_value=(TEST_DATA / 'C016_calibration.xml').open(),
     ):
-        load_camera_calibrations('camera-model', (1280, 720))
+        camera_matrix, distance_coefficients = load_camera_calibrations(
+            'camera-model',
+            (1280, 720),
+        )
+
+        expected_camera_matrix = [
+            [1.2522471126459800e+03, 0.0, 6.1724173580467107e+02],
+            [0.0, 1.2522471126459800e+03, 3.0883837638358619e+02],
+            [0.0, 0.0, 1.0],
+        ]
+        assert camera_matrix == expected_camera_matrix
+
+        expected_distance_coefficients = [[
+            -3.0075450734301268e-01,
+            5.2034021230266936e-01,
+            -2.7004158974004307e-03,
+            1.0791605890294514e-02,
+            -9.2571589159495127e-01,
+        ]]
+        assert distance_coefficients == expected_distance_coefficients
