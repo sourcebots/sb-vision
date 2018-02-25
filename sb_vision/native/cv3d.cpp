60e89479fcf5513e5e8b5fe0e78d6895d57257f1
Peter Law <PeterJCLaw@gmail.com>
2018-02-25 01:47:15 +0000

    Add some helper prints
    While I wouldn't normally commit debugging code, these will be
    removed by the compiler and took a while to get right, so they
    seem useful to have around.

--- sb_vision/native/cv3d.cpp
+++ sb_vision/native/cv3d.cpp
@@ -18,6 +18,38 @@ extern "C" {
 
 #include "opencv2/opencv.hpp"
 
+void print_array(const int rows, const int cols, const float values[]) {
+    printf("[\n");
+    for (int i=0; i<rows; i++) {
+        printf("  [");
+        for (int j=0; j<cols; j++) {
+            int idx = (i * cols) + j;
+            printf("%f", values[idx]);
+            if (j+1 < cols) {
+                printf(", ");
+            }
+        }
+        printf("],\n");
+    }
+    printf("]\n");
+}
+
+void print_mat(cv::Mat& mat) {
+    printf("[\n");
+    for (int i=0; i<mat.rows; i++) {
+        printf("  [");
+        for (int j=0; j<mat.cols; j++) {
+            float * val = (float *)mat.ptr(i, j);
+            printf("%f", *val);
+            if (j+1 < mat.cols) {
+                printf(", ");
+            }
+        }
+        printf("],\n");
+    }
+    printf("]\n");
+}
+
 #define COPY_INTO(mat, data) \
     memcpy(mat.ptr(), data, mat.rows * mat.cols * sizeof(float));
 
@@ -30,6 +62,11 @@ int solve_pnp(
     float rvec[],
     float tvec[]
 ) {
+    // printf("object_points: "); print_array(4, 3, object_points);
+    // printf("image_points: "); print_array(4, 2, image_points);
+    // printf("camera_matrix: "); print_array(3, 3, camera_matrix);
+    // printf("dist_coeffs: "); print_array(5, 1, dist_coeffs);
+
     cv::Mat object_points_mat(4, 3, CV_32FC1);
     COPY_INTO(object_points_mat, object_points);
 
@@ -41,6 +78,11 @@ int solve_pnp(
 
     cv::Mat dist_coeffs_mat(5, 1, CV_32FC1);
     COPY_INTO(dist_coeffs_mat, dist_coeffs);
+
+    // printf("object_points_mat: "); print_mat(object_points_mat);
+    // printf("image_points_mat: "); print_mat(image_points_mat);
+    // printf("camera_matrix_mat: "); print_mat(camera_matrix_mat);
+    // printf("dist_coeffs_mat: "); print_mat(dist_coeffs_mat);
 
     cv::Mat rvec_mat(3, 1, CV_32FC1);
     cv::Mat tvec_mat(3, 1, CV_32FC1);
