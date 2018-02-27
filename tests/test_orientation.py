361ce0935cc83e803f00956705613c8b85a0a387
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-02-27 13:53:09 +0000

    make wrapping checks optional

--- tests/test_orientation.py
+++ tests/test_orientation.py
@@ -15,52 +15,62 @@ CALIBRATIONS = Path(__file__).parent.parent / 'calibrations' / 'tecknet_rotation
 TEST_IMAGES = [
     (
         '0rot_x-45rot_y1.1z.png',
+        False,
         Orientation(rot_x=0, rot_y=math.radians(-45), rot_z=0),
     ),
     (
         '-45rot_x0rot_y1.1z.png',
+        False,
         Orientation(rot_x=math.radians(-45), rot_y=0, rot_z=0),
     ),
     (
         '-22.5rot_x0rot_y0.6z.png',
+        False,
         Orientation(rot_x=math.radians(-22.5), rot_y=0, rot_z=0),
     ),
     (
         '0rot_x-22.5rot_y0.6z.png',
+        False,
         Orientation(rot_x=0, rot_y=math.radians(-22.5), rot_z=0),
     ),
     (
         '0rot_x0rot_y0.55z.png',
+        False,
         Orientation(rot_x=0, rot_y=0, rot_z=0),
     ),
     (
         '-90rot_z.png',
+        False,
         Orientation(rot_x=0, rot_y=0, rot_z=math.radians(270)),
     ),
     (
         '90rot_z.png',
+        False,
         Orientation(rot_x=0, rot_y=0, rot_z=math.radians(90)),
     ),
     (
         '180rot_z.png',
+        True,
         Orientation(rot_x=0, rot_y=0, rot_z=math.radians(180)),
     ),
     (
         '135rot_z.png',
+        False,
         Orientation(rot_x=0, rot_y=0, rot_z=math.radians(135)),
     ),
     (
         '45rot_z.png',
+        False,
         Orientation(rot_x=0, rot_y=0, rot_z=math.radians(45)),
     ),
 ]
 
 
 @pytest.mark.parametrize(
-    "photo, expected_orientation",
+    "photo, allow_wrapping, expected_orientation",
     TEST_IMAGES,
 )
-def test_image_coordinates(photo, expected_orientation):
+def test_image_coordinates(photo, allow_wrapping, expected_orientation):
     """Make sure that this particular file gives these particular tokens."""
     camera = FileCamera(CALIBRATIONS / photo, camera_model='C016')
     vision = Vision(camera)
@@ -72,8 +82,13 @@ def test_image_coordinates(photo, expected_orientation):
     def assert_angle(angle_radians, expected_degrees, message):
         expected_degrees = approx_ang(math.degrees(expected_degrees))
         # Check both +0 and +360 so approx can cover the jump between -180 and 180
-        assert math.degrees(angle_radians) == expected_degrees or math.degrees(angle_radians) + 360 == expected_degrees, \
-            message
+        if allow_wrapping:
+            assert \
+                math.degrees(angle_radians) == expected_degrees or \
+                math.degrees(angle_radians) + 360 == expected_degrees, \
+                message
+        else:
+            assert math.degrees(angle_radians) == expected_degrees, message
 
     rot_x, rot_y, rot_z = token.orientation
 
